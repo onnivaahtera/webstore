9f51d4fd1b71ec056024eb6f6352599665b2d504
onnivaahtera <onni.vaahtera@gmail.com>
2022-12-08 22:08:16 +0200

    resetted /api/nextauth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,80 +1,30 @@
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth, { type NextAuthOptions } from "next-auth";
+import DiscordProvider from "next-auth/providers/discord";
+// Prisma adapter for NextAuth, optional and can be removed
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-export default NextAuth({
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      // The name to display on the sign in form (e.g. 'Sign in with...')
-      name: "my-project",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        email: {
-          label: "email",
-          type: "email",
-          placeholder: "jsmith@example.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        const payload = {
-          email: credentials?.email,
-          password: credentials?.password,
-        };
+import { env } from "../../../env/server.mjs";
+import { prisma } from "../../../server/db/client";
 
-        const res = await fetch("http://localhost:3000/api/tokens", {
-          method: "POST",
-          body: JSON.stringify(payload),
-          headers: {
-            "Content-Type": "application/json",
-            "Accept-Language": "en-US",
-          },
-        });
-
-        const user = await res.json();
-        if (!res.ok) {
-          throw new Error(user.exception);
-        }
-        // If no error and we have user data, return it
-        if (res.ok && user) {
-          return user;
-        }
-
-        // Return null if user data could not be retrieved
-        return null;
-      },
-    }),
-    // ...add more providers here
-  ],
-  secret: process.env.JWT_SECRET,
-  pages: {
-    signIn: "/account/login",
-  },
-
+export const authOptions: NextAuthOptions = {
+  // Include user.id on session
   callbacks: {
-    async jwt({ token, user, account }) {
-      if (account && user) {
-        return {
-          ...token,
-          accessToken: user.data.token,
-          refreshToken: user.data.refreshToken,
-        };
+    session({ session, user }) {
+      if (session.user) {
+        session.user.id = user.id;
       }
-
-      return token;
-    },
-
-    async session({ session, token }) {
-      session.user.accessToken = token.accessToken;
-      session.user.refreshToken = token.refreshToken;
-      session.user.accessTokenExpires = token.accessTokenExpires;
-
       return session;
     },
   },
-  // Enable debug messages in the console if you are having problems
-  debug: process.env.NODE_ENV === "development",
-});
+  // Configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    DiscordProvider({
+      clientId: env.DISCORD_CLIENT_ID,
+      clientSecret: env.DISCORD_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);

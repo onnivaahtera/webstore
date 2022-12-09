01a6fce78d1aac960fe41db1739f69cfddbf006e
onnivaahtera <onni.vaahtera@gmail.com>
2022-12-10 01:10:40 +0200

    no more type error

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,74 +1,67 @@
 import NextAuth, { type NextAuthOptions } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { verify } from "argon2";
 
-import { env } from "../../../env/server.mjs";
 import { prisma } from "../../../server/db/client";
+import { loginSchema } from "../../../auth/validation/auth";
 
-export const authOptions: NextAuthOptions = {
-  // Include user.id on session
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.id = user.id;
-        token.email = user.email;
-        token.name = user.name;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if(token) {
-        session.user.id = token.id;
-      }
-    },
-    return session
-  },
-  
-  // Configure one or more authentication providers
-  adapter: PrismaAdapter(prisma),
+export const AuthOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
+      name: "credentials",
       credentials: {
-        email: { label: "email", type: "email" },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "jsmith@gmail.com",
+        },
         password: { label: "Password", type: "password" },
       },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
+      authorize: async (credentials) => {
+        try {
+          const { email, password } = await loginSchema.parseAsync(credentials);
 
-        const user = await prisma.user.findUnique({
-          where: { email: credentials?.email },
-        });
+          const result = await prisma.user.findFirst({
+            where: { email },
+          });
 
-        if (!user) {
+          if (!result) return null;
+
+          if (password !== result.password) {
+            return null;
+          }
+
+          return { id: result.id, email, username: result.name } as any;
+        } catch {
           return null;
         }
-
-        const validPass = "test";
-
-        if (!validPass) {
-          return null;
-        }
-
-        return {
-          id: user.id,
-          email: user.email,
-          username: user.name,
-        };
       },
     }),
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
-    // ...add more providers here
   ],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.userId = user.id;
+        token.email = user.email;
+        token.username = user.username;
+      }
+
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user.userId = token.userId;
+        session.user.email = token.email;
+        session.user.username = token.username;
+      }
+
+      return session;
+    },
+  },
+  jwt: {
+    maxAge: 15 * 24 * 30 * 60, // 15 days
+  },
+  secret: "super-secret",
 };
 
-export default NextAuth(authOptions);
+export default NextAuth(AuthOptions);
--- src/server/trpc/router/user.ts
+++ src/server/trpc/router/user.ts
@@ -1,26 +1,26 @@
 import { publicProcedure, router } from "../trpc";
-import z from 'zod'
+import z from "zod";
 
 export const userRouter = router({
-    register: publicProcedure
+  register: publicProcedure
     .input(
-        z.object({
-            name: z.string(),
-            email: z.string(),
-            password: z.string(),
-            fname: z.string(),
-            lname: z.string(),
-        })
+      z.object({
+        name: z.string(),
+        email: z.string(),
+        password: z.string(),
+        fname: z.string(),
+        lname: z.string(),
+      })
     )
-    .mutation(({ctx, input}) => {
-        return ctx.prisma.user.create({
-            data: {
-                name: `${input.name}`,
-                email: `${input.email}`,
-                password: `${input.password}`,
-                fname: `${input.fname}`,
-                lname: `${input.lname}`,
-            }
-        })
+    .mutation(({ ctx, input }) => {
+      return ctx.prisma.user.create({
+        data: {
+          name: `${input.name}`,
+          email: `${input.email}`,
+          password: `${input.password}`,
+          fname: `${input.fname}`,
+          lname: `${input.lname}`,
+        },
+      });
     }),
-})
+});

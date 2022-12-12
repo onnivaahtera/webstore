333889da6841973b0ec524f411690b4d3c83c104
onnivaahtera <onni.vaahtera@gmail.com>
2022-12-12 19:12:43 +0200

    header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -116,8 +116,6 @@ const Header = () => {
           </button>
         </div>
 
-        <Link href="/account">Account</Link>
-
         {/* Cart */}
         <div className="absolute right-8 top-5 p-2 lg:right-4 ">
           <Link href="/product/cart">

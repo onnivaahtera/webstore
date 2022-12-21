62de1c088febf637fbd4aff1db527a080240b853
onnivaahtera <onni.vaahtera@gmail.com>
2022-12-21 11:05:11 +0200

    search

--- src/pages/product/search.tsx
+++ src/pages/product/search.tsx
@@ -1,6 +1,8 @@
 import { useRouter } from "next/dist/client/router";
 import type { FC } from "react";
 import { trpc } from "../../utils/trpc";
+import Image from "next/image";
+import Link from "next/link";
 
 const Search: FC = () => {
   const router = useRouter();
@@ -14,9 +16,21 @@ const Search: FC = () => {
     <div>
       {searchResults.data?.map((value, key) => (
         <div key={key}>
-          <div className="flex">
-            <div className="p-2">{value.name}</div>
-            <div className="p-2">{value.price}</div>
+          <div className="m-2">
+            <Link href={`/product/${value.name}`}>
+              <div className="flex flex-row border-2 border-gray-700">
+                <Image
+                  className="py-2"
+                  src={value.image}
+                  alt="product"
+                  height={100}
+                  width={100}
+                  unoptimized
+                />
+                <div className="p-2">{value.name}</div>
+                <div className="p-2">{value.price}</div>
+              </div>
+            </Link>
           </div>
         </div>
       ))}

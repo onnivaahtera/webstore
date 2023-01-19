db99139f564e4145f8ba905edbc713ae61a0e197
onnivaahtera <onni.vaahtera@gmail.com>
2023-01-19 14:26:24 +0200

    product page add to cart

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -2,11 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import type { FC } from "react";
+import { useShoppingCart } from "../../context/ShoppingCartContext";
 import { trpc } from "../../utils/trpc";
+import Custom404 from "../404";
 
 const Product: FC = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { increaseCartQuantity } = useShoppingCart();
 
   const product = trpc.product.getProduct.useQuery({ url: `${id}` });
 
@@ -16,18 +19,18 @@ const Product: FC = () => {
 
   const item = product.data[0];
 
-  if (!item) return <div>No product</div>;
+  if (!item) return <Custom404 />;
 
   return (
     <>
       <Head>
-        <title>{item?.name}</title>
+        <title>{item.name}</title>
       </Head>
 
       <div className="product-wrapper flex">
         <div className="product-image mx-32 mt-16 rounded-md">
           <Image
-            src={`${item?.image}`}
+            src={`${item.image}`}
             alt="product image"
             width={400}
             height={400}
@@ -37,19 +40,19 @@ const Product: FC = () => {
         <div className="flex-col">
           <div className="product-name p-2">
             <div className="h-14 w-44 rounded-md p-2 text-3xl text-white">
-              <p>{item?.name}</p>
+              <p>{item.name}</p>
             </div>
           </div>
           <div className="product-price px-2 text-white">
             <div className="h-18 w-44 rounded-md">
               <div className="p-2">
-                <p className="text-2xl">{item?.price}€</p>
+                <p className="text-2xl">{item.price}€</p>
                 <p className="text-xs text-gray-500">includes vat. 24%</p>
               </div>
             </div>
           </div>
           <div className="p-2">
-            <button>
+            <button onClick={() => increaseCartQuantity(id)}>
               <div className="h-10 w-36 rounded bg-blue-500 p-2 text-white shadow-md shadow-blue-500">
                 Add to cart
               </div>
@@ -59,7 +62,7 @@ const Product: FC = () => {
       </div>
       <div className="text-center text-white">
         <h2 className="m-6 text-2xl ">Description</h2>
-        <div className="text-lg">{item?.desc}</div>
+        <div className="text-lg">{item.desc}</div>
       </div>
     </>
   );

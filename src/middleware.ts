b4d8bc551293d5aad946e8fe8153cdaaad4cfbe2
onnivaahtera <onni.vaahtera@gmail.com>
2023-03-20 21:51:59 +0200

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,17 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  const session = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+  if (req.nextUrl.pathname.startsWith("/account/admin")) {
+    if (!session || session.role !== "Admin")
+      return NextResponse.redirect(new URL("/account/login", req.url));
+  }
+  if (req.nextUrl.pathname.startsWith("/account/info")) {
+    if (!session || session.role !== "Customer")
+      return NextResponse.redirect(new URL("/account/login", req.url));
+  }
+}

60018f1f4f65ae6746632b7523857626e65b9250
onnivaahtera <onni.vaahtera@gmail.com>
2023-03-27 21:20:01 +0300

    fixed footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer: FC = () => {
   return (
     <footer className="flex h-[65px] items-center justify-center border-t-2 border-background2">
       <div className="flex flex-row items-center justify-center">
-        <span className="text-center">Copyright @ 2023: Onni Vaahtera |</span>
+        <span className="text-center">Copyright © 2023 Onni Vaahtera |</span>
         <Link href="https://github.com/onnivaahtera/webstore">
           <FaGithub className="ml-1 text-xl hover:opacity-70" />
         </Link>
